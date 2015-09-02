53105b198712cc37e8f840188bb0797bb6e77d91
Tigerwander <jiang_hu_lu@163.com>
2015-09-02 15:49:02 +0800

    delete edge_all.m

--- code/benchmark/scripts/edge_all.m
+++ code/benchmark/scripts/edge_all.m
@@ -1,26 +1,24 @@
-function edge_all(datasets,set_type,gt_set)
-	if nargin < 1
-		datasets='Pascal_07_test';
-		set_type='Main';
-		gt_set='test.txt';
-	end
+function edge_all()
+	p = get_paths();
+	data_dir = sprintf('%s/%d_%f',p.data_dir,p.feature_len,p.miss_rate);
+	p.data_path = data_dir
 
-	set_file=fullfile(root_dir,'datasets',datasets,'ImageSets',set_type,gt_set);
+	set_file=fullfile(p.dataset_dir,p.test_set,'ImageSets',p.test_sub_set,p.test_gt);
 	im_ids=load(set_file);
 	image_num=length(im_ids);
 
-	rank_dir=sprintf('%s/benchmark/edge_bbox_10',root_dir);
-	if ~exist(rank_dir,'dir')
-		mkdir(rank_dir);
+	edge_bbox_dir=sprintf('%s/edge_bbox',data_dir);
+	if ~exist(edge_bbox_dir,'dir')
+		mkdir(edge_bbox_dir);
 	end
 
 	matlabpool(8);
 	parfor ii=1:image_num
-		bbox_file=sprintf('%s/%06d.mat',rank_dir,im_ids(ii));
+		bbox_file=sprintf('%s/%06d.mat',edge_bbox_dir,im_ids(ii));
 		if exist(bbox_file,'file')
 			continue
 		else
-			bbox=edge(datasets,set_type,im_ids(ii));
+			bbox=edge(p,im_ids(ii));
 			parsave(bbox_file,bbox);
 		end
 	end
